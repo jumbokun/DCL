6329c1def863df366fd5a79ba6e3b4f54d5a9c12
Bozhen Zhu <bzhu@corinth>
2023-11-26 17:12:13 +0100

    Add runss ignore

--- download_IU_XRay.py
+++ download_IU_XRay.py
@@ -9,3 +9,6 @@ with open('/root/.kaggle/kaggle.json', 'w') as file:
     json.dump(api_token, file)
 
 !chmod 600 ~/.kaggle/kaggle.json
+
+# use command kaggle datasets download -d raddar/chest-xrays-indiana-university
+# Refer to: https://www.kaggle.com/datasets/raddar/chest-xrays-indiana-university/
